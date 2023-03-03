a1a52aee173f3239d02ec9d017bfb4697e08732d
shivam <12shivamtiwari219@gmail.com>
2023-03-03 20:09:34 +0530

    timer

--- src/Error-Time-Accuracy/Error_Time_Accuracy.jsx
+++ src/Error-Time-Accuracy/Error_Time_Accuracy.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Error_Time_Accuracy() {
+export default function Error_Time_Accuracy({ timeLimit, showlogic }) {
+
+  const [error, setError] = useState(0);
+
+
+
   return (
     <>
-
-      <div className="errors-time-accuracy-container">
-        <div className="cont max-w-[50%] mt-8 mx-auto flex justify-center">
-
-          {/* <div className="errors w-[100px] h-[100px] text-center shadow-2xl rounded-xl">
-            <h1 className='text-lg font-extrabold font-mono'>WPM</h1>
-          </div> */}
-
-          <div className="errors w-[100px] h-[100px] text-center shadow-2xl rounded-xl">
-            <h1 className='text-lg font-extrabold font-mono'>ERRORS</h1>
+      {
+        showlogic ?
+          <div className="errors-time-accuracy-container">
+            <div className="cont max-w-[50%] mt-8 mx-auto flex justify-center">
+              <div className="errors w-[100px] h-[100px] text-center shadow-2xl rounded-xl">
+                <h1 className='text-lg font-extrabold font-mono'>ERRORS</h1>
+                <span className='text-[56px] leading-none font-mono'>{error}</span>
+              </div>
+              <div className="time w-[100px] h-[100px] text-center mx-5 shadow-2xl rounded-xl">
+                <h1 className='text-lg font-extrabold font-mono'>TIME</h1>
+                <span className='flex justify-evenly text-[46px] font-mono'>{timeLimit} <span>S</span> </span>
+              </div>
+              <div className="accuracy w-[100px] h-[100px] text-center shadow-2xl rounded-xl">
+                <h1 className='text-lg font-extrabold font-mono'>ACCURACY</h1>
+              </div>
+            </div>
           </div>
-          <div className="time w-[100px] h-[100px] text-center mx-5 shadow-2xl rounded-xl">
-            <h1 className='text-lg font-extrabold font-mono'>TIME</h1>
+          :
+          <div className="errors-time-accuracy-container">
+            <div className="cont max-w-[50%] mt-8 mx-auto flex justify-center">
+              <div className="errors w-[100px] h-[100px] text-center shadow-2xl rounded-xl">
+                <h1 className='text-lg font-extrabold font-mono'>ERRORS</h1>
+                <span className='text-[56px] leading-none font-mono'>{error}</span>
+              </div>
+              <div className="errors w-[100px] h-[100px] mx-5 text-center shadow-2xl rounded-xl">
+                <h1 className='text-lg font-extrabold font-mono'>WPM</h1>
+                <span className='text-[46px] font-mono'>50</span>
+              </div>
+              <div className="accuracy w-[100px] h-[100px] text-center shadow-2xl rounded-xl">
+                <h1 className='text-lg font-extrabold font-mono'>ACCURACY</h1>
+              </div>
+            </div>
           </div>
-          <div className="accuracy w-[100px] h-[100px] text-center shadow-2xl rounded-xl">
-            <h1 className='text-lg font-extrabold font-mono'>ACCURACY</h1>
-          </div>
-        </div>
-      </div>
 
+
+      }
 
     </>
   )
--- src/Test/Test.jsx
+++ src/Test/Test.jsx
@@ -4,15 +4,34 @@ import paragraph from '../JSON/paragraph.json'
 
 export default function Test() {
 
-    const randomParagraph = Math.floor(Math.random() * 10);
     const [rand, setRand] = useState(0);
-    useEffect(
-        () => {
-
-        }, [rand])
-    console.log(rand);
     const [input, setInput] = useState('');
-    if (paragraph[rand].paragraph == input) {
+    const [testarea, setTextarea] = useState(true);
+    const [showBtn, setHide] = useState(true)
+    const [showlogic, setLogic] = useState(true)
+    let [timeLimit, setTimeLimit] = useState(10);
+
+    let Interval = () => {
+        let x = setInterval(() => {
+            timeLimit--;
+            setTimeLimit(timeLimit);
+            if (timeLimit == 0) {
+                clearInterval(x)
+                setTimeLimit(10);
+                setHide(true)
+                setTextarea(true)
+                setLogic(false)
+            }
+        }, 1000)
+    }
+
+    const handleButton = () => {
+
+        const randomParagraph = Math.floor(Math.random() * paragraph.length);
+        setTextarea(false);
+        setRand(randomParagraph);
+        setHide(false);
+        Interval();
 
     }
 
@@ -24,20 +43,33 @@ export default function Test() {
                 </div>
             </div>
 
-            <Error_Time_Accuracy />
+            <Error_Time_Accuracy timeLimit={timeLimit} showlogic={showlogic} />
 
             <div className="fixPara w-full block">
                 <div className="paraGraph max-w-[50%] shadow-2xl p-4 mt-3 mx-auto text-justify font-mono text-base font-bold">
-                    <p>{paragraph[rand].paragraph}</p>
+                    <p onCopy={(e) => {
+                        e.preventDefault()
+                        return false
+                    }}>{paragraph[rand].paragraph}</p>
                 </div>
             </div>
 
             <div className='inputParagraph'>
                 <div className='input  max-w-[40%] p-4 mt-3 mx-auto'>
-                    <textarea name="" value={input} id="" cols="30" rows="10" onChange={(e) => setInput(e.target.value)} className='w-full h-[200px] rounded-xl p-2 shadow-2xl'></textarea>
+                    <textarea name="" placeholder='Start typing here...' disabled={testarea} onPaste={(e) => {
+                        e.preventDefault()
+                        return false
+                    }} onCopy={(e) => {
+                        e.preventDefault()
+                        return false
+                    }}
+                        value={input} id="" cols="30" rows="10" onChange={(e) => setInput(e.target.value)} className='w-full h-[170px] rounded-xl p-2 shadow-2xl'></textarea>
                 </div>
             </div>
-            {/* <button onClick={() => setRand(randomParagraph)}>Click</button> */}
+
+            <div className="btn text-center">
+                <button onClick={handleButton} className={showBtn ? 'w-[100px] p-1 shadow-lg text-white font-extrabold text-xl font-mono rounded-md' : "hidden"}>Start</button>
+            </div>
         </>
     )
 }
